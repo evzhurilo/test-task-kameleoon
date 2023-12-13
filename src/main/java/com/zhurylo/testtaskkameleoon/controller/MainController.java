b64b935698b2477d82284402a44c33fbf10dd71f
jack <evzhurilo@gmail.com>
2023-12-13 16:36:34 -0400

    solve issue_2

--- src/main/java/com/zhurylo/testtaskkameleoon/controller/MainController.java
+++ src/main/java/com/zhurylo/testtaskkameleoon/controller/MainController.java
@@ -3,14 +3,16 @@ package com.zhurylo.testtaskkameleoon.controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping("/api/v1/main")
 public class MainController {
 
     @GetMapping("/page")
-    public String getMainPage(){
-        return "main-page";
+    public ModelAndView getMainPage() {
+        ModelAndView modelAndView = new ModelAndView("main-page");
+        return modelAndView;
     }
 
 }
